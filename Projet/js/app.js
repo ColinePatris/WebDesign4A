e541721d78c2681020c4d4b90abc31f4f8c63c11
Jeremiah Ravenor <jmkduong@yahoo.fr>
2017-02-28 19:55:51 +0100

    Controllerfucked up

--- Projet/js/app.js
+++ Projet/js/app.js
@@ -8,7 +8,6 @@ angular.module( 'BPD' )
             } )
             .when( "/List", {
                 templateUrl: "Partials/List.html",
-                controller: 'BaltiController'
             } )
 			.when( "/Home", {
                 templateUrl: "Partials/Home.html",
@@ -17,41 +16,3 @@ angular.module( 'BPD' )
                    	redirectTo: "Home"
             } )
     } ] );
-
-app.controller( 'BaltiController', [ '$http','$scope', function ( $scope, $http ) {
-    console.log( "BaltiController", $http );
-    var ctrl = this;
-    this.selected = 0;
-    this.arrests = [];
-
-    $http.get( 'https://data.baltimorecity.gov/resource/3i3v-ibrt.json' )
-        .success( function ( result ) {
-            console.log( 'callback', this );
-            console.log( result );
-            $scope.arrests = result;
-        } );
-} ] );
-
-app.controller('pageBController',[
-'$scope','$routeParams',
-function($scope,$routeParams){
-	$scope.test1="du texte";
-	$scope.message=$routeParams.msg;
-	$routeParams.msg;
-	var products={
-		"product_1":{
-			"name":"phone",
-			"price":25.95
-		},
-		"product_2":{
-			"name":"gun",
-			"price":545.95
-		},
-		"product_3":{
-			"name":"Floppy",
-			"price":15.95
-		}
-	};
-	$scope.product=products[$routeParams.msg];
-}
-]);
