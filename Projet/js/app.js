bf31bc25f5b0f94ac568e9c055bebc5a79d1067b
ColinePatris <coline.patris@epfedu.fr>
2017-03-07 00:53:37 +0100

    Avec Ombres, coins et données lisibles

--- Projet/js/app.js
+++ Projet/js/app.js
@@ -19,9 +19,12 @@ app.controller( 'BaltiController', [ '$scope', '$http', function ( $scope, $http
     var ctrl = this;
     this.countSexF = 0;
     this.countSexM = 0;
-	this.sexArray=[[0],[0]];
-	this.ageArray=[0,0,0,0,0];
-	this.raceArray=[0,0,0,0,0];
+    this.sexArray = [
+        [ 0 ],
+        [ 0 ]
+    ];
+    this.ageArray = [ 0, 0, 0, 0, 0 ];
+    this.raceArray = [ 0, 0, 0, 0, 0 ];
     this.countRaceA = 0;
     this.countRaceB = 0;
     this.countRaceI = 0;
@@ -56,31 +59,37 @@ app.controller( 'BaltiController', [ '$scope', '$http', function ( $scope, $http
         .success( function ( result ) {
             ctrl.bufferArrests = result;
             ctrl.arrests = result;
-			angular.forEach( ctrl.bufferArrests, function ( value, key ) {
-			if ( value.sex == "F" ) ctrl.countSexF++;
-			if ( value.sex == "M" ) ctrl.countSexM++;
-			if ( value.race == "A" ) ctrl.countRaceA++;
-			if ( value.race == "B" ) ctrl.countRaceB++;
-			if ( value.race == "I" ) ctrl.countRaceI++;
-			if ( value.race == "U" ) ctrl.countRaceU++;
-			if ( value.race == "W" ) ctrl.countRaceW++;
-			if ( value.age <= 25 && value.age >= 15 ) ctrl.countAge1525++;
-			if ( value.age <= 35 && value.age >= 26 ) ctrl.countAge2635++;
-			if ( value.age <= 50 && value.age >= 36 ) ctrl.countAge3650++;
-			if ( value.age <= 65 && value.age >= 51 ) ctrl.countAge5165++;
-			if ( value.age >= 65 ) ctrl.countAge6500++;
-			} );
-			ctrl.sexArray=[[ctrl.countSexF],[ctrl.countSexM]];
-			ctrl.ageArray=[ctrl.countAge1525,ctrl.countAge2635,ctrl.countAge3650,ctrl.countAge5165,ctrl.countAge6500];
-			ctrl.raceArray=[ctrl.countRaceA,ctrl.countRaceB,ctrl.countRaceI,ctrl.countRaceU,ctrl.countRaceW];
-			console.log(ctrl.raceArray);
-    $scope.rLabels = [ "Asians", "Blacks",  "Indians", "Unknown","Whites" ];
-    $scope.rData = ctrl.raceArray;
-    $scope.aLabels = [ "15-25", "26-35", "36-50", "51-65", "Above 65" ];
-    $scope.aData = ctrl.ageArray;
-    $scope.sLabels = [ '' ];
-    $scope.sSeries = [ 'Female', 'Male' ];
-    $scope.sData = ctrl.sexArray;
+            angular.forEach( ctrl.bufferArrests, function ( value, key ) {
+                if ( value.sex == "F" ) ctrl.countSexF++;
+                if ( value.sex == "M" ) ctrl.countSexM++;
+                if ( value.race == "A" ) ctrl.countRaceA++;
+                if ( value.race == "B" ) ctrl.countRaceB++;
+                if ( value.race == "I" ) ctrl.countRaceI++;
+                if ( value.race == "U" ) ctrl.countRaceU++;
+                if ( value.race == "W" ) ctrl.countRaceW++;
+                if ( value.age <= 25 && value.age >= 15 ) ctrl.countAge1525++;
+                if ( value.age <= 35 && value.age >= 26 ) ctrl.countAge2635++;
+                if ( value.age <= 50 && value.age >= 36 ) ctrl.countAge3650++;
+                if ( value.age <= 65 && value.age >= 51 ) ctrl.countAge5165++;
+                if ( value.age >= 65 ) ctrl.countAge6500++;
+            } );
+            ctrl.sexArray = [ ctrl.countSexF, ctrl.countSexM ];
+            ctrl.ageArray = [ ctrl.countAge1525, ctrl.countAge2635, ctrl.countAge3650, ctrl.countAge5165, ctrl.countAge6500 ];
+            ctrl.raceArray = [
+                [ ctrl.countRaceA ],
+                [ ctrl.countRaceB ],
+                [ ctrl.countRaceI ],
+                [ ctrl.countRaceU ],
+                [ ctrl.countRaceW ]
+            ];
+            console.log( ctrl.raceArray );
+            $scope.sLabels = [ "Female", "Male" ];
+            $scope.sData = ctrl.sexArray;
+            $scope.aLabels = [ "15-25 yo", "26-35 yo", "36-50 yo", "51-65 yo", "Above 65 yo" ];
+            $scope.aData = ctrl.ageArray;
+            $scope.rLabels = [ '' ];
+            $scope.rSeries = [ 'Asians', 'Blacks', 'Indians', 'Unknown', 'Whites' ];
+            $scope.rData = ctrl.raceArray;
         } );
     this.load = function () {
         ctrl.arrests = [];
